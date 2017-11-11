2bd801bdb09e607b8b6dc43ce58e8e97b0a6dcf8
Dominic Watson <domwatson@live.co.uk>
2017-11-11 10:53:57 +0100

    Fix for GET requests not working in link-http

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { ApolloModule, Apollo } from 'apollo-angular';
-import { HttpLinkModule, HttpLinkHandler } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink, HttpLinkHandler } from './http-link';
 
 const uri = 'https://www.graphqlhub.com/graphql';
 
 @NgModule({
+  providers: [
+    HttpLink,
+  ],
   exports: [
     ApolloModule,
     HttpClientModule,
-    HttpLinkModule,
   ],
 })
 export class GraphqlModule {
-  constructor(private apollo: Apollo, private http: HttpClient) {
+  constructor(private apollo: Apollo, private httpLink: HttpLink) {
     this.apollo.create({
-      link: new HttpLinkHandler(http, { uri, method: 'GET' }),
+      link: httpLink.create({ uri, method: 'GET' }),
       cache: new InMemoryCache(),
     });
   }
